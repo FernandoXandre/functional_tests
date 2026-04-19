7e7de60d8edf503c79b51978d87a8a01c79d6d42
FFernando_Alexandre <jcf3057@gmail.com>
2026-04-19 18:51:58 -0300

    Refatorando o Teste de Unidade (boa prática: método de teste deve testar apenas uma coisa)

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,12 @@
-def check_for_row_in_list_table(self, row_text):
-   table = self.browser.find_element_by_id('id_list_table')
-   rows = table.find_elements_by_tag_name('tr')
-   self.assertIn(row_text, [row.text for row in rows])
+    def test_can_save_a_POST_request(self):
+        self.client.post('/', data={'item_text': 'A new list item'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_POST(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
